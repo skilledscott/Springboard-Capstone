ba76168ef1c13c320eb8d57232f8c14271357185
Scott Gibson <scottchgibson@gmail.com>
2023-02-05 16:34:15 -0800

    Finish prototype functionality
    Added more functions to utils to reduce the amount of
    code in one function in model.py.
    
    Altered prototype model.py file to load one requested
    image, get predictions on it, load targets from dataset
    annotations file, display mean Average precision, and then
    display image with bounding boxes drawn.

--- prototype/model.py
+++ prototype/model.py
@@ -1,69 +1,67 @@
 import torch
 from torchvision.io import read_image
-from torchvision.transforms.functional import to_pil_image
 from torchvision.models.detection import fasterrcnn_resnet50_fpn_v2, FasterRCNN_ResNet50_FPN_V2_Weights
-from torchvision.utils import draw_bounding_boxes
 
-import pandas as pd
 import numpy as np
-
 from time import time
-
-from utils import (load_labels_df, 
-    get_true_labels, 
+from utils import (load_labels_df,
+    filter_preds,
+    get_true_labels,
+    convert_labels_to_tensor,
     get_mean_average_precision,
     print_metric,
     draw_preds
 )
 
-# loads the PyTorch Faster_RCNN model
-# returns model, preprocessing transform, and weight categories
+# Constants
+DATASET_PATH = '../dataset_small/'
+IMAGE_NAME = 'img74.jpg'
+DEVICE = 'gpu'
+
+
 def load_model():
+    '''
+    load_model: Loads the faster_rcnn model from PyTorch with default
+                weights.
+                
+                The preprocessing returned is to be run on images before
+                inputting them into the model for prediction.
+
+                The meta_categories returned are an array of categories,
+                so if the model returns a label of 3, that's referring to
+                meta_categories[3] i.e. 'car'.
+    
+    Returns model, preprocess, meta_categories
+    '''
+
     weights = FasterRCNN_ResNet50_FPN_V2_Weights.DEFAULT
     model = fasterrcnn_resnet50_fpn_v2(weights=weights, box_score_thresh=0.9)
     model.eval()
 
     preprocess = weights.transforms()
-    weights_categories = weights.meta["categories"]
+    meta_categories = weights.meta["categories"]
 
-    return model, preprocess, weights_categories
+    return model, preprocess, meta_categories
 
 
-# device can be ['cpu', 'cuda']
-def get_batch_preds(img_names, ann_df, device='cpu', verbose=True):
-    if verbose:
-        print('Running model on: [{}] - {} images'.format(device, len(img_names)))
+def get_preds(model, preprocess, meta_categories, img_names, device='cpu'):
+    print('Running model on: [{}] - {} images'.format(device, len(img_names)))
     
-    pipeline_start_time = time()
-
-    # check to make sure PyTorch sees the gpu before loading model and 
-    # batch of images
-    if not torch.cuda.is_available():
-        print('\tNo device detected, running on cpu')
-        device = 'cpu'
-
-    # load model
-    model, preprocess, weights_categories = load_model()
-    model.to(device)
-
-    if verbose:
-        print('\tModel loaded         - {} seconds'.format(time() - pipeline_start_time))
+    prediction_start_time = time()
 
     # run images through model in batches of 5 at a time
     batch_size = 5
-    total = len(img_names)
     left = 0
     preds = []
-    prediction_start_time = time()
-    while left < total:
+    while left < len(img_names):
         # load batch of images from dataset
         right = left + batch_size
         imgs = [read_image(DATASET_PATH + name) for name in img_names[left:right]]
-        if device == 'cuda':
-            imgs = [img.cuda() for img in imgs]
 
-        # create batch using preprocess on input batch of images
+        # preprocess images and load them to gpu if necessary
         batch = [preprocess(img) for img in imgs]
+        if device == 'cuda':
+            batch = [img.cuda() for img in batch]
 
         # get predictions for the batch of images
         with torch.no_grad():
@@ -74,39 +72,58 @@ def get_batch_preds(img_names, ann_df, device='cpu', verbose=True):
 
         left += batch_size
 
-    if verbose:
-        print('\tPredictions finished - {} seconds'.format(time() - prediction_start_time))
-        print('\n\tTotal pipeline       - {} seconds'.format(time() - pipeline_start_time))
+    # filter preds and keep only the 'car' labels
+    filtered_preds = filter_preds(preds, meta_categories, device)
 
-    # put all output tensors on cpu for ease of scoring later
-    if device != 'cpu':
-        for i in range(len(preds)):
-            preds[i]['boxes'] = preds[i]['boxes'].to('cpu')
-            preds[i]['labels'] = preds[i]['labels'].to('cpu')
-            preds[i]['scores'] = preds[i]['scores'].to('cpu')
-
-    return preds
+    print('\tPredictions finished - {} seconds'.format(time() - prediction_start_time))
+    
+    return filtered_preds
 
 
-# set constant dataset path for input images
-DATASET_PATH = '../dataset_small/test/'
+def get_targets(img_names, ann_df, meta_categories, device):
+    targets = []
+    for name in img_names:
+        labels, boxes = get_true_labels(name, ann_df)
+        labels = convert_labels_to_tensor(labels, meta_categories)
+        targets.append({
+            'boxes': boxes if device == 'cpu' else boxes.cuda(),
+            'labels': labels if device == 'cpu' else labels.cuda()
+        })
+    
+    return targets
 
-# load annotations for dataset
-ann_df = load_labels_df(DATASET_PATH)
 
-# create a list of input images and run model to get
-# batch predictions
-# currently gets one random image, and runs preds on that
-n_images = 1
-img_nums = np.random.randint(1, 201, n_images)
-img_names = ['img{}.jpg'.format(num) for num in img_nums]
-preds = get_batch_preds(img_names, ann_df, device='cuda', verbose=True)
+if __name__ == '__main__':
+    # Make sure that cuda is available if running on gpu
+    device = DEVICE
+    if DEVICE == 'gpu':
+        assert torch.cuda.is_available(), \
+            'Error: DEVICE set to gpu, no gpu available. Change DEVICE to cpu in constants.'
+        device = 'cuda'
 
-# get map
-metric = get_mean_average_precision(img_names, ann_df, preds)
+    # load model, preprocess, and meta_categories
+    model, preprocess, meta_categories = load_model()
+    model.to(device)
 
-# print metric evaluation
-print_metric(metric)
+    # load annotations for dataset
+    ann_df = load_labels_df(DATASET_PATH)
 
-# draw preds and display/save to file (display for now)
-draw_preds(preds)
+    # create an array of images to read from the dataset
+    img_names = [IMAGE_NAME]
+
+    # get predictions from the model on the array of img_names
+    preds = get_preds(model, preprocess, meta_categories, img_names, device)
+
+    # get target, true labels from the annotations df
+    targets = get_targets(img_names, ann_df, meta_categories, device)
+
+    # get map comparing preds to true labels
+    metric = get_mean_average_precision(preds, targets)
+
+    # print metric evaluation
+    print_metric(metric)
+
+    # draw preds and display/save to file (display for now)
+    draw_preds(DATASET_PATH, img_names, preds)
+
+    print('\nDone')
--- prototype/utils.py
+++ prototype/utils.py
@@ -1,63 +1,140 @@
 import torch
 from torchmetrics.detection.mean_ap import MeanAveragePrecision
+from torchvision.io import read_image
+from torchvision.transforms.functional import to_pil_image
+from torchvision.utils import draw_bounding_boxes
 
 import matplotlib.pyplot as plt
 import pandas as pd
 from pprint import pprint
+from PIL import Image
 
-# reads in the dataset _annotations.csv file
-# returns a pandas DataFrame
-# MOVE TO UTILS
+
 def load_labels_df(path):
     '''
-    load_labels_df: 
+    load_labels_df: Uses pandas to read the csv at the location, 
+                    '<path>/_annotations.csv', and returns the read 
+                    DataFrame.
+    
+    :param path: str, image dataset path where .csv file is stored
+
+    Returns ann_df -> pd.DataFrame
     '''
     # read in _annotations.csv file with bboxes and labels into ann_df
     names = ['img_name', 'x1', 'y1', 'x2', 'y2', 'label']
     ann_df = pd.read_csv(path + '_annotations.csv', header=None, names=names)
     return ann_df
 
-# get's the true labels of an image from the annotations df
-# returns labels, bounding box coordinates
-# MOVE TO UTILS
+
 def get_true_labels(img_name, ann_df):
+    '''
+    get_true_labels: Filters ann_df for all rows for the given img_name.
+                     Returns a tensor of labels, and a tensor of bounding boxes.
+    
+    :param img_name: str
+    :param ann_df: pd.DataFrame
+
+    Returns labels -> list[str], boxes -> torch.tensor(list[list[float]])
+    '''
     img_df = ann_df[ann_df['img_name'] == img_name]
     boxes_df = img_df[['x1', 'y1', 'x2', 'y2']]
     
     labels = list(img_df['label'])
-    labels = torch.tensor([3 for _ in labels])
     boxes = torch.tensor(boxes_df.to_numpy())
 
     return labels, boxes
 
+def convert_labels_to_tensor(labels, meta_categories):
+    '''
+    convert_labels_to_tensor: Convert a list of strings to their integer
+                              index location in meta_categories. Will return a
+                              tensor of integers stored on the cpu.
 
-# only compatible with cuda tensors at the moment
-# returns the mean average precision metric based on the annotations df and
-# the model predictions
-# returns the computed metric
-# MOVE TO UTILS
-def get_mean_average_precision(img_names, ann_df, preds):
-    targets = [] # one dict per image
+    :param labels: list[str], bounding box labels, like 'car', or 'bike'
+    :param meta_categories: list[str], each str corresponds to an output label.
+                       if meta_categories[3] = 'car' -> 'car' = 3.
+    
+    Returns labels -> torch.tensor(list[int])
+    '''
 
-    for img_name in img_names:
-        labels, boxes = get_true_labels(img_name, ann_df)
-        targets.append({
-            'boxes': boxes,
-            'labels': labels
-        })
+    int_labels = []
+    for label in labels:
+        for index in range(len(meta_categories)):
+            if meta_categories[index] == label:
+                int_labels.append(index)
+                break
+            
+    return torch.tensor(int_labels)
+
+
+def filter_preds(preds, meta_categories, device):
+    # filter preds and keep only the 'car' labels
+    car_label = [i for i in range(len(meta_categories)) if
+                    meta_categories[i] == 'car'][0]
+    filtered_preds = []
+    for pred in preds:
+        keep_indeces = torch.tensor([i for i in range(len(pred['labels']))
+                                        if pred['labels'][i] == car_label]).to(device)
+        filtered_pred = {
+            'boxes': torch.index_select(pred['boxes'], 0, keep_indeces),
+            'labels': torch.index_select(pred['labels'], 0, keep_indeces),
+            'scores': torch.index_select(pred['scores'], 0, keep_indeces)
+        }
+        filtered_preds.append(filtered_pred)
+    
+    return filtered_preds
+
+
+def get_mean_average_precision(preds, targets):
+    '''
+    get_mean_average_precision: Inits and updates a PyTorch MeanAveragePrecision
+                                metric and returns that metric to the user. The
+                                metric can be visualized by calling metric.compute().
+                                Or by calling print_metric from utils.py.
+    
+    :param img_names: list[str], list of image names, should be 1-to-1 with preds
+    :param ann_df: pd.DataFrame, df of true labels for images
+    :param preds: list[dict['boxes', 'labels', 'scores']], each image from img_names
+                  corresponds to an entry in this list
+
+    Returns metric -> MeanAveragePrecision object
+    '''
 
     metric = MeanAveragePrecision()
     metric.update(preds, targets)
     return metric
 
 
+# print_metric: Uses pprint and metric.compute() to display metric information
+# to the console.
 def print_metric(metric):
     print('\nMean Average Precision:')
     pprint(metric.compute())
-    return metric
 
 
-# to be implemented...
-def draw_preds(preds):
+def draw_preds(dataset_path, img_names, preds):
+    '''
+    draw_preds: Uses plt.imshow to display the bounding boxes and labels from
+                preds on the first input image.
+    
+    :param dataset_path: str, path to the dataset of images
+    :param img_names: list[str], each image should match one-to-one with the preds
+                      list
+    :param preds: list[dict['boxes', 'labels', 'scores']]
+    '''
+
     print('\nDrawing bounding boxes...')
-    print(preds[0])
+
+    boxes = preds[0]['boxes']
+    labels = ['car' for _ in preds[0]['labels']]
+
+    img = read_image(dataset_path + img_names[0])
+    boxes_img = draw_bounding_boxes(image=img, 
+                                    boxes=boxes.to('cpu'), 
+                                    labels=labels,
+                                    colors='red',
+                                    width=4,
+                                    font='arial',
+                                    font_size=24)
+    plt.imshow(to_pil_image(boxes_img))
+    plt.show()
